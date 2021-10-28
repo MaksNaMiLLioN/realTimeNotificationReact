50167979db15dff605a2a62c0ca1e9c3498c59d0
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-10-28 19:41:56 +0600

    Setup Socket Server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './app.css'
 import Navbar from './components/navbar/Navbar'
 import Card from './components/card/Card'
 import {posts} from './data'
+const { io } = require("socket.io-client");
 
 function App() {
   const [username, setUsername] = useState('')
   const [user, setUser] = useState('')
+
+  useEffect(() => {
+    const socket = io("http://localhost:4000")
+    console.log(socket)
+  }, [])
 
 
   return (
--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,18 @@
 const { Server } = require("socket.io");
 
-const io = new Server({ /* options */ });
+const io = new Server({ 
+  cors:{
+    origin: "http://localhost:3000"
+  }
+ });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("someone has connected...")
+
+  socket.on("disconnect", () => {
+    console.log("someone has left")
+  })
 });
 
-io.listen(3000);
+io.listen(4000);
 
-ßß
