17e0b77708aafa85e4fd9bf0a49faa03fbdb2838
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-10-29 05:25:41 +0600

    Setup Notifications

--- socket/index.js
+++ socket/index.js
@@ -1,4 +1,4 @@
-const { Server } = require("socket.io");
+const { Server, Socket } = require("socket.io");
 
 const io = new Server({ 
   cors:{
@@ -6,11 +6,38 @@ const io = new Server({
   }
  });
 
+ let onlineUsers = []
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some(user=>user.username === username) && onlineUsers.push({username, socketId})
+}
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter( user =>{
+    user.socketId !== socketId
+  })
+}
+
+const getUser = (username) => {
+  return onlineUsers.find(user => user.username === username)
+}
+
 io.on("connection", (socket) => {
-  console.log("someone has connected...")
+
+    socket.on("newUser", (username) => {
+      addNewUser(username, socket.id)
+    })
+
+  socket.on("sendNotification", ({senderName, receiverName, type})=>{
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getNotification", {
+      senderName,
+      type
+    });
+  });
 
   socket.on("disconnect", () => {
-    console.log("someone has left")
+    removeUser(socket.id)
   })
 });
 
